667cd0d164c8029d32bb2969034261543a1de265
asliddin <asliddin>
2025-07-22 10:49:52 +0300

    initial commit

---
+++ app/app.js
@@ -0,0 +1,229 @@
+Asliddin [22.07.2025 10:31]
+// 1 - masala 3 tasi
+
+
+// function showAge(age) {
+//     console.log(sizning yoshingiz ${age})
+// }
+
+// showAge(20)
+
+// 2
+
+// const showAge = function (age) {
+//     console.log(Sizning yoshingiz ${age});
+// };
+
+// showAge(20);
+
+// 3
+
+// const showAge = (age) => console.log(sizning yoshingiz ${age});
+
+// showAge(20)
+
+//   ///////////
+// 2 - masala 3tasi
+
+// function addNumbers(a, b) {
+//     console.log(a + b );
+// }
+// addNumbers(3, 7);
+
+// 2
+
+// const addNumbers = function( a, b){
+//     console.log(a + b)
+// }
+// addNumbers(3, 7)
+
+// 3
+
+// const addNumbers = (a, b) => console.log(a + b)
+
+// addNumbers( 3, 7)
+
+// ///////////
+
+// 3- masala 3tasi
+
+// function greet(name) {
+//     console.log(Salom, ${name}!);
+// }
+
+// greet('Asliddin');
+
+// 2
+
+// const greet = function(name){
+//     console.log(Salom ${name}!)
+// }
+// greet('Ulugbek')
+
+// 3
+
+// const greet = (name) => console.log(Salom ${name}!)
+
+// greet('Asliddin')
+
+
+// ////////
+
+// 4 -masala
+// function fullName(firstName, lastName){
+//     console.log(${firstName}  ${lastName})
+// }
+// fullName('Asliddin', 'Fozilov')
+
+// 2
+
+// const fullName = function(firstName, lastName){
+//     console.log(${firstName}  ${lastName})
+// }
+// fullName('asliddin', 'Fozilov')
+
+// 3
+
+// const fullName = (firstName, lastName) => console.log(${firstName}  ${lastName})
+// fullName('asliddin', 'Fozilov')
+
+// ///////
+// 5 masala
+// function rectanglePerimeter(length, width) {
+//     console.log(2 * (length + width));
+// }
+// rectanglePerimeter(5, 3);
+
+// 2
+
+// const rectanglePerimeter = function(length, width){
+//     console.log(2 * (length + width));
+// }
+// rectanglePerimeter(5, 3)
+
+// 3
+
+// const rectanglePerimeter =(length, width) => console.log(2 * (length + width))
+// rectanglePerimeter(5, 3)
+
+// 3
+
+// ////////
+// 6 -masala
+// function triangleSides(a, b, c) {
+//     console.log(1-tomon: ${a})
+//     console.log(2-tomon: ${b})
+//     console.log(3-tomon: ${c})
+// }
+
+// triangleSides(3, 4, 5);
+
+// 2
+
+// const triangleSides = function(a, b, c){
+//     console.log(1-tomon: ${a})
+//     console.log(2-tomon: ${b})
+//     console.log(3-tomon: ${c})
+// }
+// triangleSides(3, 4, 5)
+
+// 3
+
+// const triangleSides = (a, b, c) => {
+//     console.log(1-tomon: ${a})
+//     console.log(2-tomon: ${b})
+//     console.log(3-tomon: ${c})
+//   };
+//   triangleSides(3, 4, 5);
+ 
+////////////
+//   7 - masala
+// function squareNumber(number){
+//     console.log(number  2)
+// }
+// squareNumber(5)
+
+// 2
+
+// const squareNumber = function(number){
+//     console.log(number  2)
+// }
+// squareNumber(5)
+
+// 3
+
+// const squareNumber = (number) => console.log(number ** 2)
+// squareNumber(5)
+
+// ///////
+// 8 -masala
+// function animmalInfo(name, age){
+//     console.log(Bu ${name}, ${age} yoshda)
+// }
+// animmalInfo('Mushuk', 2)
+
+// 2
+
+// const animmalInfo = function(name, age){
+//     console.log(Bu ${name}, ${age} yoshda)
+// }
+// animmalInfo('Mushuk',2)
+
+// 3
+
+// const animmalInfo = (name, age) => console.log(Bu ${name}, ${age} yoshda)
+// animmalInfo('Mushuk',2)
+
+// ////////
+// 9 masala
+// function sayHello(world){
+//     console.log(${world})
+// }
+// sayHello('Salom Dunyo!')
+
+// 2
+
+// const sayHello = function(world){
+//     console.log(${world})
+// }
+// sayHello('Salom Dunyo!')
+
+// 3
+
+// const sayHello = (world) => console.log(${world})
+// sayHello('Salom Dunyo!')
+
+// /////////
+// 10 masala
+// function printNames(name1, name2, name3, name4, name5){
+//     console.log(${name1})
+//     console.log(${name2})
+//     console.log(${name3})
+//     console.log(${name4})
+//     console.log(${name5})
+// }
+// printNames('sher','Vali', 'Gulchapchap', 'Zilola', 'Javohir')
+
+// 2
+
+// const printNames = function(name1, name2, name3, name4, name5){
+//     console.log(${name1})
+//     console.log(${name2})
+//     console.log(${name3})
+//     console.log(${name4})
+//     console.log(${name5})
+// }
+// printNames('Ali','Vali', 'Gulbahor', 'Zilola', 'Javohir')
+
+// 3
+
+//  Asliddin [22.07.2025 10:31]
+// const printNames = (name1, name2, name3, name4, name5) => {
+//     console.log(${name1})
+//     console.log(${name2})
+//     console.log(${name3})
+//     console.log(${name4})
+//     console.log(${name5})
+
+// }
+// printNames('Ali','Gulchapchap', 'Gulbahor', 'Zilola', 'Javohir')
